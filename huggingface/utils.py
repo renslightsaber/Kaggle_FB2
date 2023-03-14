c7e56b83235837b3d83dd29a9c76d8dcfbd7bcd3
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-14 10:28:24 +0900

    Update utils.py

--- huggingface/utils.py
+++ huggingface/utils.py
@@ -24,6 +24,18 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
 
+
+########### compute metrics #################
+from sklearn.metrics import accuracy_score, f1_score
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    f1 = f1_score(preds, labels, average = "weighted")
+    acc = accuracy_score(preds, labels)
+    return {"acc": acc, "f1": f1}
+
+
 ########### Scheduler #################
 import torch.optim as optim
 
