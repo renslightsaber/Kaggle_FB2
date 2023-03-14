f505f9f6fa5a0a768c19a817a9a8d39dc9592670
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-14 10:16:48 +0900

    Update train.py

--- huggingface/train.py
+++ huggingface/train.py
@@ -102,7 +102,7 @@ def define():
     p.add_argument('--min_lr', type = float, default = 1e-6, help="Min LR")
     p.add_argument('--weight_decay', type = float, default = 1e-6, help="Weight Decay")
 
-    p.add_argument('--"n_accumulate"', type = int, default = 1, help="n_accumulate")
+    p.add_argument('--n_accumulate', type = int, default = 1, help="n_accumulate")
     p.add_argument('--max_grad_norm', type = int, default = 1000, help="max_grad_norm")
     p.add_argument('--grad_clipping', type = bool, default = False, help="Gradient Clipping")
     p.add_argument('--device', type = str, default = "cuda", help="CUDA or MPS or CPU?")
