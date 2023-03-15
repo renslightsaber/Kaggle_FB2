af0b811a3d37af01158e4e03299c99e738db3f0f
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2023-03-16 00:47:44 +0900

    Update model.py

--- lightning/model.py
+++ lightning/model.py
@@ -15,6 +15,8 @@ import torch.nn as nn
 from torch.optim import lr_scheduler
 from torch.utils.data import Dataset, DataLoader
 
+from utils import *
+
 # Pytorch Lightning
 import pytorch_lightning as pl
 
